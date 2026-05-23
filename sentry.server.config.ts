6c1ec011a10318eaa99984f30d8c206ee9443217
NifemiSoneye <nifemisoneye@gmail.com>
2026-05-23 10:49:35 -0700

    Setup kindeauth and sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://89ee412de2dbff82358f7624e03e8c06@o4511237520490496.ingest.us.sentry.io/4511441007869952",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
