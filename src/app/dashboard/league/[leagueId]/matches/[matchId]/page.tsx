a8af0217a5d39f011bee7cd5ce2501ba9f77172f
NifemiSoneye <nifemisoneye@gmail.com>
2026-06-18 14:10:02 -0700

    Added match server component

---
+++ src/app/dashboard/league/[leagueId]/matches/[matchId]/page.tsx
@@ -0,0 +1,43 @@
+import {
+  getCachedMatch,
+  getCachedSofascoreMatchData,
+} from "@/lib/cached-football-data";
+import { LEAGUES } from "@/lib/constants";
+import MatchClient from "./MatchClient";
+type Props = {
+  params: Promise<{ leagueId: string; matchId: string }>;
+};
+
+export default async function MatchPage({ params }: Props) {
+  const { leagueId, matchId } = await params;
+
+  const leagueIdNum = Number(leagueId);
+  const matchIdNum = Number(matchId);
+
+  const league = LEAGUES.find((l) => l.id === leagueIdNum);
+
+  const match = await getCachedMatch(matchIdNum);
+
+  const isFinished = match?.status === "FINISHED";
+
+  let sofascoreData = null;
+
+  if (isFinished && league?.sofascore) {
+    sofascoreData = await getCachedSofascoreMatchData(
+      league.sofascore.tournamentId,
+      league.sofascore.seasonId,
+      match.homeTeam.name,
+      match.awayTeam.name,
+      match.utcDate,
+      matchIdNum,
+    );
+  }
+
+  return (
+    <MatchClient
+      match={match}
+      sofascoreData={sofascoreData}
+      leagueId={leagueIdNum}
+    />
+  );
+}
